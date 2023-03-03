bc05200b49ef8d05942cbdd9693c3d46af7451fe
zhaopan <syszhaopan@gmail.com>
2023-03-04 01:04:37 +0800

    add(frpd): ...

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM nginx:latest
-MAINTAINER zhaopan
-
-EXPOSE 80
--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 # 初始化网络
-docker network create --subnet=172.18.0.0/16 Vpc01
+docker network create --subnet=172.18.0.0/16 devops_proxy
